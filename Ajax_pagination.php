b614c2e74cc9ad9521921111f204863026451e50
CodexWorld <nityamaity87@gmail.com>
2019-09-19 22:23:32 +0530

    Update Ajax_pagination.php

--- Ajax_pagination.php
+++ Ajax_pagination.php
@@ -13,34 +13,34 @@
 class Ajax_pagination{
 
 	var $base_url           = ''; // The page we are linking to
-	var $total_rows  		= ''; // Total number of items (database results)
-	var $per_page	 		= 10; // Max number of items you want shown per page
-	var $num_links			=  2; // Number of "digit" links to show before/after the currently viewed page
-	var $cur_page	 		=  0; // The current page being viewed
-	var $first_link   		= 'First';
-	var $next_link			= '&#187;';
-	var $prev_link			= '&#171;';
-	var $last_link			= 'Last';
-	var $uri_segment		= 3;
-	var $full_tag_open		= '<div class="pagination">';
-	var $full_tag_close		= '</div>';
-	var $first_tag_open		= '';
+	var $total_rows  	= ''; // Total number of items (database results)
+	var $per_page	 	= 10; // Max number of items you want shown per page
+	var $num_links		=  2; // Number of "digit" links to show before/after the currently viewed page
+	var $cur_page	 	=  0; // The current page being viewed
+	var $first_link   	= 'First';
+	var $next_link		= '&#187;';
+	var $prev_link		= '&#171;';
+	var $last_link		= 'Last';
+	var $uri_segment	= 3;
+	var $full_tag_open	= '<div class="pagination">';
+	var $full_tag_close	= '</div>';
+	var $first_tag_open	= '';
 	var $first_tag_close	= '&nbsp;';
-	var $last_tag_open		= '&nbsp;';
-	var $last_tag_close		= '';
-	var $cur_tag_open		= '&nbsp;<b>';
-	var $cur_tag_close		= '</b>';
-	var $next_tag_open		= '&nbsp;';
-	var $next_tag_close		= '&nbsp;';
-	var $prev_tag_open		= '&nbsp;';
-	var $prev_tag_close		= '';
-	var $num_tag_open		= '&nbsp;';
-	var $num_tag_close		= '';
+	var $last_tag_open	= '&nbsp;';
+	var $last_tag_close	= '';
+	var $cur_tag_open	= '&nbsp;<b>';
+	var $cur_tag_close	= '</b>';
+	var $next_tag_open	= '&nbsp;';
+	var $next_tag_close	= '&nbsp;';
+	var $prev_tag_open	= '&nbsp;';
+	var $prev_tag_close	= '';
+	var $num_tag_open	= '&nbsp;';
+	var $num_tag_close	= '';
 	var $target             = '';
-    var $anchor_class		= '';
-    var $show_count         = true;
-    var $link_func          = 'getData';
-    var $loading            = '.loading';
+    	var $anchor_class	= '';
+    	var $show_count         = true;
+    	var $link_func          = 'getData';
+    	var $loading            = '.loading';
 
 	/**
 	 * Constructor
@@ -91,7 +91,7 @@ class Ajax_pagination{
 
 		// Is there only one page? Hm... nothing more to do here then.
 		if ($num_pages == 1){
-            $info = 'Showing : ' . $this->total_rows;
+            		$info = 'Showing : ' . $this->total_rows;
 			return $info;
 		}
 
